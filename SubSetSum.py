46b66ab973db6659cbcbd496174317a6d6d3b2fd
joshuav2 <joshuav2@illinois.edu>
2019-10-12 04:37:24 -0500

    SubSetSum

---
+++ SubSetSum.py
@@ -0,0 +1,21 @@
+#Recursive algorithm to find sum T in set X of POSITIVE integers
+#Assume 0 is part of every set i.e empty set is part of every set
+
+def subSetSum(X,T):
+  if T == 0:
+    return True
+  elif T < 0 or not X:
+    return False
+  else:
+    for x in X:
+      X_copy = X.copy()
+      X_copy.discard(x)
+      With = subSetSum(X_copy, T - x)
+      Without = subSetSum(X_copy, T)
+      return With or Without
+
+
+X = set([2,4,5,7,8,10])
+T = 26
+
+print(subSetSum(X,T))
